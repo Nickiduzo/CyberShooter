f77455485d9a3e75cd1d4a1cf137289277407a3e
Nickiduzo <79377105+Nickiduzo@users.noreply.github.com>
2024-10-16 19:37:47 +0300

    Animation and scripting
    - added new animations
    - added new character moving

--- Assets/Scripts/Bot/Moving.cs
+++ Assets/Scripts/Bot/Moving.cs
@@ -8,15 +8,26 @@ public class Moving : MonoBehaviour
     [SerializeField] private float mouseSensetivity = 100f;
     [SerializeField] private float lookLimit = 85f;
 
-    [SerializeField] private float speed = 3f;
-    [SerializeField] private float shiftSpeed = 6f;
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float sprintSpeed = 6f;
+    [SerializeField] private float jumpHeight = 2f;
 
     private float verticalLookRotation = 0f;
 
     private float horizontalInput;
     private float verticalInput;
+
+    private bool isGrounded = true;
+    [SerializeField] private float checkDistance;
+    [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField] private Rigidbody rb;
+    
+
     private void Update()
     {
+        //CheckOnGround();
+        //Jump();
         Move();
         ChangeAnimation();
         RotateBot();
@@ -27,23 +38,58 @@ public class Moving : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
+        float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
+
+        if(speed == sprintSpeed && IsMoving())
+        {
+            anim.SetBool("isRun", true);
+        }
+        else
+        {
+            anim.SetBool("isRun", false);
+        }
+    
         transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
     }
 
+    //private void Jump()
+    //{
+    //    if(Input.GetButtonDown("Jump") && isGrounded)
+    //    {
+    //        rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+    //        isGrounded = false;
+    //    }
+    //}
+
+    //private void CheckOnGround()
+    //{
+    //    isGrounded = Physics.Raycast(transform.position, Vector3.down, checkDistance, groundLayer);
+    //    if(isGrounded)
+    //    {
+    //        anim.SetBool("isJump", true);
+    //    }
+    //    else
+    //    {
+    //        anim.SetBool("isJump", false);
+    //    }
+    //}
     private void ChangeAnimation()
     {
         if(horizontalInput != 0 || verticalInput != 0)
         {
-            anim.SetBool("isMoving", true);
+            anim.SetBool("isMove", true);
         }
 
         if(horizontalInput == 0 && verticalInput == 0)
         {
-            anim.SetBool("isMoving", false);
+            anim.SetBool("isMove", false);
         }
     }
-
+    private bool IsMoving()
+    {
+        return (horizontalInput != 0 || verticalInput != 0);
+    }
     // rotation of player
     private void RotateBot()
     {
