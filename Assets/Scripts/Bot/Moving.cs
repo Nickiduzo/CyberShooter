e37de9f47a6b83f6dc0b81f8fc6c26b2d3991743
Nickiduzo <79377105+Nickiduzo@users.noreply.github.com>
2024-10-18 20:02:36 +0300

    Light Refactoring

--- Assets/Scripts/Bot/Moving.cs
+++ Assets/Scripts/Bot/Moving.cs
@@ -1,108 +1,103 @@
 using UnityEngine;
-
 public class Moving : MonoBehaviour
 {
     [SerializeField] private Animator anim;
     [SerializeField] private Transform cameraTransform;
 
-    [SerializeField] private float mouseSensetivity = 100f;
+    [SerializeField] private float mouseSensitivity = 100f;
     [SerializeField] private float lookLimit = 85f;
 
-    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float walkSpeed = 3f;
     [SerializeField] private float sprintSpeed = 6f;
-    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float jumpForce = 5f;
 
     private float verticalLookRotation = 0f;
 
     private float horizontalInput;
     private float verticalInput;
 
-    private bool isGrounded = true;
-    [SerializeField] private float checkDistance;
+    private bool isGrounded;
+    [SerializeField] private float checkDistance = 1.1f;
     [SerializeField] private LayerMask groundLayer;
 
-    [SerializeField] private Rigidbody rb;
-    
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
-        //CheckOnGround();
-        //Jump();
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            anim.SetTrigger("Dance");
-        }
-        Move();
-        ChangeAnimation();
-        RotateBot();
+        print(isGrounded);
+        HandleInput();
+        AnimateMovement();
+        RotatePlayer();
     }
 
-    private void Move()
+    private void FixedUpdate()
+    {
+        Move();
+        GroundCheck();
+    }
+
+    private void HandleInput()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
-
-        if(speed == sprintSpeed && IsMoving())
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            anim.SetBool("isRun", true);
-        }
-        else
-        {
-            anim.SetBool("isRun", false);
-        }
-    
-        transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
-        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
-    }
-
-    //private void Jump()
-    //{
-    //    if(Input.GetButtonDown("Jump") && isGrounded)
-    //    {
-    //        rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-    //        isGrounded = false;
-    //    }
-    //}
-
-    //private void CheckOnGround()
-    //{
-    //    isGrounded = Physics.Raycast(transform.position, Vector3.down, checkDistance, groundLayer);
-    //    if(isGrounded)
-    //    {
-    //        anim.SetBool("isJump", true);
-    //    }
-    //    else
-    //    {
-    //        anim.SetBool("isJump", false);
-    //    }
-    //}
-    private void ChangeAnimation()
-    {
-        if(horizontalInput != 0 || verticalInput != 0)
-        {
-            anim.SetBool("isMove", true);
+            Jump();
         }
 
-        if(horizontalInput == 0 && verticalInput == 0)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            anim.SetBool("isMove", false);
+            anim.SetTrigger("Dance");
         }
     }
-    private bool IsMoving()
+
+    private void Move()
     {
-        return (horizontalInput != 0 || verticalInput != 0);
+        float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        // Преобразуем направление движения в мировые координаты
+        Vector3 moveDirection = transform.TransformDirection(direction) * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + moveDirection);
     }
-    // rotation of player
-    private void RotateBot()
+
+    private void Jump()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensetivity * Time.fixedDeltaTime;
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false; // Сбросим состояние при прыжке
+    }
+
+    private void GroundCheck()
+    {
+        // Используем Raycast для проверки касания земли
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, checkDistance, groundLayer);
+    }
+
+    private void AnimateMovement()
+    {
+        bool isMoving = horizontalInput != 0 || verticalInput != 0;
+        anim.SetBool("isMove", isMoving);
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift) && isMoving;
+        anim.SetBool("isRun", isRunning);
+    }
+
+    private void RotatePlayer()
+    {
+        // Получаем вращение по оси Y для поворота персонажа
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
         transform.Rotate(Vector3.up * mouseX);
 
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensetivity * Time.fixedDeltaTime;
+        // Вращаем камеру по оси X (вверх-вниз)
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
         verticalLookRotation -= mouseY;
-        verticalLookRotation = Mathf.Clamp(verticalLookRotation,-lookLimit,lookLimit);
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -lookLimit, lookLimit);
 
         cameraTransform.localRotation = Quaternion.Euler(verticalLookRotation, 0, 0);
     }
